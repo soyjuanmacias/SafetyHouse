dbd274d6e628cf2b10d80e38dfa2d70d39f81468
Juan Macias <yo@soyjuanmacias.com>
2017-08-01 20:48:41 +0200

    First commit, express server ready! :D

---
+++ server/config/express.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const session = require('express-session')
+const passport = require('passport')
+const logger = require('morgan')
+const layouts = require('express-ejs-layouts')
+const config = require('./config.js')
+const mongoose = require('mongoose')
+
+module.exports = function(app) {
+
+  mongoose.connect(config.db)
+
+  app.set('views', config.rootPath + 'views')
+  app.set('view engine', 'ejs')
+  app.use(logger('dev'))
+  app.use(bodyParser.json())
+  app.use(bodyParser.urlencoded({
+    extended: false
+  }))
+  app.use(cookieParser())
+  app.use(express.static(config.rootPath + 'public'))
+  app.use(layouts)
+  app.use(session({
+    secret: 'basicsecret',
+    resave: false,
+    saveUninitialized: true,
+  }))
+  app.use(passport.initialize())
+  app.use(passport.session())
+
+
+  app.use(function(req, res, next) {
+    res.locals.user = req.user;
+    res.locals.title = 'BASIC BOILERPLATE!!!'
+    console.log('Imprimo res.locals.users =>');
+    console.log(res.locals.user);
+    next()
+  })
+}
