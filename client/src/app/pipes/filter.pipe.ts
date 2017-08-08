93dc47de780099ac1326b4a37f26c84f21d4a7b5
Juan Macias <yo@soyjuanmacias.com>
2017-08-08 19:01:06 +0200

    Filters and search working good, starting notifications

--- client/src/app/pipes/filter.pipe.ts
+++ client/src/app/pipes/filter.pipe.ts
@@ -5,7 +5,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FilterPipe implements PipeTransform {
 
-  transform(items: any[], field: string, value: string): any[] {
+  transform(items: any[], field: string , value: string): any[] {
     if (!items) {
       return [];
     }
