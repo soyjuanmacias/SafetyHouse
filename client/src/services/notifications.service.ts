502d78a140465ceccaccbf17ede12f6a2c1db1d4
Juan Macias <yo@soyjuanmacias.com>
2017-08-09 15:50:53 +0200

    prev to start with materialize

--- client/src/services/notifications.service.ts
+++ client/src/services/notifications.service.ts
@@ -2,20 +2,18 @@ import { Observable } from 'rxjs/Observable';
 import * as io from 'socket.io-client';
 
 export class NotificationsService {
-  private url = 'http://localhost:3000';
+  private url = 'http://localhost:8080';
   private socket;
 
   getNotifications() {
     let observable = new Observable(observer => {
+      console.log('Cargando el servicio')
       this.socket = io(this.url);
       this.socket.on('notifications', (data) => {
         console.log('Imprimo data en servicio notifications =>')
         console.log(data)
         observer.next(data);
       });
-      return () => {
-        this.socket.disconnect();
-      }
     })
     return observable;
   }
