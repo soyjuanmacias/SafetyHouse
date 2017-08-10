d75e7bb998afa11b40d58c6be0904a509b6bd4f4
Juan Macias <yo@soyjuanmacias.com>
2017-08-10 20:33:57 +0200

    Complaint first design, add complaint first desgign

--- client/src/app/js/init.js
+++ client/src/app/js/init.js
@@ -1,8 +1,18 @@
-(function($){
-  $(function(){
+(function($) {
+  $(function() {
 
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
+    $('.timepicker').pickatime({
+      default: 'now',
+      twelvehour: false,
+      donetext: 'OK',
+      cleartext: 'Clear',
+      canceltext: 'Cancel',
+      autoclose: false,
+      ampmclickable: true,
+      aftershow: function() {}
+    });
 
   });
 })(jQuery);
